ab1ca69c6b1bb1f3c9cbbb7e0b9c248697df1a55
Daniel Gu <99457610+DanielGu23@users.noreply.github.com>
2022-03-07 04:27:07 +0000

    Changed variable names

--- Sketch.java
+++ Sketch.java
@@ -8,8 +8,8 @@ public class Sketch extends PApplet {
   */
 
 // Create the time and location variables
-float catHeadX;
-float catHeadY;
+float fltCatHeadX;
+float fltCatHeadY;
 int intHours;
 int intMinutes;
 int intSeconds;
@@ -17,62 +17,62 @@ int intSeconds;
   public void settings() {
     // Size of sketchpad and randomly generating values for the cat head position
     size(400, 400);
-    catHeadX = random(0, width);
-    catHeadY = random(0, height);
+    fltCatHeadX = random(0, width);
+    fltCatHeadY = random(0, height);
     intHours = hour();
     intMinutes = minute();
     intSeconds = second();
     
-    // Prints out current time
+    // Print out current time
     System.out.print ("The current time is "+intHours + " hours and ");
     System.out.print (+intMinutes + " minutes and ");
     System.out.print (+intSeconds + " seconds");
   }
 
   public void draw() {
-    float headSize = width/2;  
+    float fltHeadSize = width/2;  
     
     // Change the background color based on the location of the cat head
     // Turquoise background
-    if(catHeadX > headSize && catHeadY > headSize){ 
+    if(fltCatHeadX > fltHeadSize && fltCatHeadY > fltHeadSize){ 
       background(0, 140, 128);
       
       // Green background
-      }else if (!(catHeadX > headSize || catHeadY > headSize)){
+      }else if (!(fltCatHeadX > fltHeadSize || fltCatHeadY > fltHeadSize)){
         background(0, 200, 0);
       
       // Pink background
-      }else if (catHeadX < headSize && catHeadY > headSize){
+      }else if (fltCatHeadX < fltHeadSize && fltCatHeadY > fltHeadSize){
         background(200, 0, 120);
       
       // Gray background
-      }else if (catHeadX > headSize && catHeadY < headSize){
+      }else if (fltCatHeadX > fltHeadSize && fltCatHeadY < fltHeadSize){
         background(123, 123, 123);
       }
     
     // Draw the head
     stroke(0);
-    ellipse(catHeadX, catHeadY, headSize, headSize);    
+    ellipse(fltCatHeadX, fltCatHeadY, fltHeadSize, fltHeadSize);    
     
     // Draw the ears
     stroke(0);
-    line((float)(catHeadX - headSize/1.5), (float)(catHeadY - headSize/1.5), (catHeadX - headSize/4), (float)(catHeadY - headSize/2.4));
-    line((float)(catHeadX - headSize/1.5), (float)(catHeadY - headSize/1.5), (float)(catHeadX - headSize/2), (float)(catHeadY - headSize/10));
-     line((float)(catHeadX + headSize/1.5), (float)(catHeadY - headSize/1.5), (catHeadX + headSize/4), (float)(catHeadY - headSize/2.4));
-    line((float)(catHeadX + headSize/1.5), (float)(catHeadY - headSize/1.5), (float)(catHeadX + headSize/2), (float)(catHeadY - headSize/10));    
+    line((float)(fltCatHeadX - fltHeadSize/1.5), (float)(fltCatHeadY - fltHeadSize/1.5), (fltCatHeadX - fltHeadSize/4), (float)(fltCatHeadY - fltHeadSize/2.4));
+    line((float)(fltCatHeadX - fltHeadSize/1.5), (float)(fltCatHeadY - fltHeadSize/1.5), (float)(fltCatHeadX - fltHeadSize/2), (float)(fltCatHeadY - fltHeadSize/10));
+     line((float)(fltCatHeadX + fltHeadSize/1.5), (float)(fltCatHeadY - fltHeadSize/1.5), (fltCatHeadX + fltHeadSize/4), (float)(fltCatHeadY - fltHeadSize/2.4));
+    line((float)(fltCatHeadX + fltHeadSize/1.5), (float)(fltCatHeadY - fltHeadSize/1.5), (float)(fltCatHeadX + fltHeadSize/2), (float)(fltCatHeadY - fltHeadSize/10));    
     
     // Draw the eyes
     stroke(0);
-    rect((float)(catHeadX - headSize/3), (float)(catHeadY - headSize/4.5), (headSize/5), (float)(headSize/6.67));
-    ellipse((float)(catHeadX - headSize/3 + headSize/10), (float)(catHeadY - headSize/4.5 + headSize/13.33), (headSize/20), (headSize/20));
-    rect((float)(catHeadX + headSize/3 - headSize/5), (float)(catHeadY - headSize/4.5), (headSize/5), (float)(headSize/6.67));  
-    ellipse((float)(catHeadX + headSize/3 - headSize/10), (float)(catHeadY - headSize/4.5 + headSize/13.33), (headSize/20), (headSize/20));
+    rect((float)(fltCatHeadX - fltHeadSize/3), (float)(fltCatHeadY - fltHeadSize/4.5), (fltHeadSize/5), (float)(fltHeadSize/6.67));
+    ellipse((float)(fltCatHeadX - fltHeadSize/3 + fltHeadSize/10), (float)(fltCatHeadY - fltHeadSize/4.5 + fltHeadSize/13.33), (fltHeadSize/20), (fltHeadSize/20));
+    rect((float)(fltCatHeadX + fltHeadSize/3 - fltHeadSize/5), (float)(fltCatHeadY - fltHeadSize/4.5), (fltHeadSize/5), (float)(fltHeadSize/6.67));  
+    ellipse((float)(fltCatHeadX + fltHeadSize/3 - fltHeadSize/10), (float)(fltCatHeadY - fltHeadSize/4.5 + fltHeadSize/13.33), (fltHeadSize/20), (fltHeadSize/20));
 
     // Draw the nose and mouth
-    triangle((catHeadX), (catHeadY - headSize/8), (catHeadX - headSize/7), (catHeadY + headSize/8), (catHeadX + headSize/7), (catHeadY + headSize/8));
+    triangle((fltCatHeadX), (fltCatHeadY - fltHeadSize/8), (fltCatHeadX - fltHeadSize/7), (fltCatHeadY + fltHeadSize/8), (fltCatHeadX + fltHeadSize/7), (fltCatHeadY + fltHeadSize/8));
     noFill();
-    arc((catHeadX + headSize/4), (catHeadY + headSize/8), (headSize/2), (headSize/2), HALF_PI, PI);
-    arc((catHeadX - headSize/4), (catHeadY + headSize/8), (headSize/2), (headSize/2), 0, HALF_PI);
+    arc((fltCatHeadX + fltHeadSize/4), (fltCatHeadY + fltHeadSize/8), (fltHeadSize/2), (fltHeadSize/2), HALF_PI, PI);
+    arc((fltCatHeadX - fltHeadSize/4), (fltCatHeadY + fltHeadSize/8), (fltHeadSize/2), (fltHeadSize/2), 0, HALF_PI);
     
     }
   
